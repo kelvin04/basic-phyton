332860c5c1851b5b0efddba2209ea2eda81da2f6
Kelvin Tertia <kelvin.tertia@nexsoft.co.id>
2021-02-13 14:58:14 +0700

    Test commit

---
+++ main.py
@@ -0,0 +1,36 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+test = "test"
+empat = 4
+lima = 5
+
+a,b,c = "hi",1,2.5
+
+
+valid = True
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('test')
+    print(empat + lima)
+    print(test + " " + "{}".format(empat))
+    print(c)
+    print(valid)
+    print(type(c))
+    print(0 % 7)
+    print(4 ** 2)
+    print(type(str(4.2)))
+    print(float('12'))
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
