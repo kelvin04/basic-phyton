91adaf7c0f7575b4045b08879ba20323126ce9ec
Kelvin Tertia <kelvin.tertia@nexsoft.co.id>
2021-02-13 15:04:56 +0700

    test commit 4 from pycharm

--- main.py
+++ main.py
@@ -33,5 +33,6 @@ if __name__ == '__main__':
     print(float('12'))
     print("commit2")
     print("commit3")
+    prin("commit 4")
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
